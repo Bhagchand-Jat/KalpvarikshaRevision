645c0239587cfc825e290ece7fd3fb39c2a5a43c
Bhagchand-Jat <156396944+Bhagchand-Jat@users.noreply.github.com>
2024-06-18 12:55:36 +0530

    Time: 3 ms (54.57%) | Memory: 5.9 MB (49.71%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.c
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+  int current=0;
+ void traverse(struct TreeNode* root, int *arr){
+    if(root==NULL)return;
+     
+    traverse(root->left,arr);
+    traverse(root->right,arr);
+    arr[current++]=root->val;
+}
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    
+    int arr[101];
+    current=0;
+    traverse(root,arr);
+    int total=current;
+ 
+    *returnSize=current;
+    int *result=(int *)calloc(current,sizeof(int));
+    while(--total>=0){
+        result[total]=arr[total];
+    }
+    return result;
+}
